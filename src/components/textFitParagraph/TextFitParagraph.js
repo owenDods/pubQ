8a865df381f4c5078b69e823234042aaf147c672
Owen Dods <owendods@gmail.coom>
2020-06-20 19:10:52 +0100

    Finalise TextFitParagraph component logic

--- src/components/textFitParagraph/TextFitParagraph.js
+++ src/components/textFitParagraph/TextFitParagraph.js
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
+const decreaseFontSize = oldPercentage => (oldPercentage - 5);
+
 export const className = 'textFitParagraph';
 
 const TextFitParagraph = ({ text }) => {
@@ -8,24 +10,54 @@ const TextFitParagraph = ({ text }) => {
 	const containerEl = useRef(null);
 	const paragraphEl = useRef(null);
 	const [ fontPercentage, setFontPercentage ] = useState(100);
-	useEffect(() => {
+	const [ shouldHideText, setShouldHideText ] = useState(false);
+	const resetAll = () => {
+
+		setFontPercentage(100);
+		setShouldHideText(false);
+
+	};
+	const checkIfTextIsOverflowing = () => {
 
 		const { height: containerHeight } = containerEl.current.getBoundingClientRect();
 		const { height: paragraphHeight } = paragraphEl.current.getBoundingClientRect();
 
-		if (paragraphHeight > containerHeight) {
+		return paragraphHeight > containerHeight;
 
-			setFontPercentage(oldPercentage => (oldPercentage - 5));
+	};
+	useEffect(() => {
+
+		if (checkIfTextIsOverflowing()) {
+
+			setShouldHideText(true);
+			setFontPercentage(decreaseFontSize);
 
 		}
 
-	});
+		return resetAll;
+
+	}, [ text ]);
+	useEffect(() => {
+
+		if (shouldHideText && checkIfTextIsOverflowing()) {
+
+			setFontPercentage(decreaseFontSize);
+
+		} else {
+
+			setShouldHideText(false);
+
+		}
+
+	}, [ fontPercentage ]);
 
 	const paragraphStyle = { fontSize: `${fontPercentage}%` };
 
+	const styleClass = shouldHideText ? `${className} ${className}--hideText` : className;
+
 	return (
 
-		<div ref={containerEl} className={className}>
+		<div ref={containerEl} className={styleClass}>
 
 			<p ref={paragraphEl} style={paragraphStyle}>{text}</p>
 
